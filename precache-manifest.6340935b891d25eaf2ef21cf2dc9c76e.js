d2a7f7713d6632064480939abc63b4a4bfe67d43
Ashish Gupta <calculatorlab@yahoo.com>
2020-10-04 21:09:36 +0530

    Updates

---
+++ precache-manifest.6340935b891d25eaf2ef21cf2dc9c76e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080119ca3d3b58c722cf50f82b8d7b33",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "401bbc96c1d2c4e55ea1",
+    "url": "/calc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d0bdc46461f463e5804",
+    "url": "/calc/static/js/2.9f1eaea1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc/static/js/2.9f1eaea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401bbc96c1d2c4e55ea1",
+    "url": "/calc/static/js/main.919b4a7d.chunk.js"
+  },
+  {
+    "revision": "5fcb3d4e0c15a3c40490",
+    "url": "/calc/static/js/runtime-main.b87305a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calc/static/media/logo.5d5d9eef.svg"
+  }
+]);
